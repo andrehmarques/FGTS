8cef9a37acb94d77e99ad924fc23134c510a2b69
Gabriel <lab53@fatec.com>
2025-03-31 08:33:59 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double salario, fgts;
+
+
+Console.WriteLine("Calcule seu FGTS!");
+
+Console.Write("Digite seu salário (R$): ");
+salario = double.Parse(Console.ReadLine()!);
+
+fgts = salario * 0.08;
+
+Console.Write($"O valor do seu FGTS é: {fgts:C2}");
